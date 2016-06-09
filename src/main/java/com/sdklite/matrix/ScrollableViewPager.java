242afb2a2474081b50e2931482d5a4440b2308b1
johnsonlee <g.johnsonlee@gmail.com>
2016-06-09 23:23:35 +0800

    Add ScrollableViewPager

---
+++ src/main/java/com/sdklite/matrix/ScrollableViewPager.java
@@ -0,0 +1,44 @@
+package com.sdklite.matrix;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * @author johnsonlee
+ */
+public class ScrollableViewPager extends ViewPager {
+
+    private boolean scrollable;
+
+    public ScrollableViewPager(final Context context) {
+        this(context, null);
+    }
+
+    public ScrollableViewPager(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+
+        final TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.ScrollableViewPager);
+        this.scrollable = ta.getBoolean(R.styleable.ScrollableViewPager_scrollable, false);
+        ta.recycle();
+    }
+
+    public boolean isScrollable() {
+        return this.scrollable;
+    }
+
+    public void setScrollable(final boolean scrollable) {
+        this.scrollable = scrollable;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(final MotionEvent event) {
+        if (this.scrollable) {
+            return super.onInterceptTouchEvent(event);
+        }
+
+        return false;
+    }
+}
